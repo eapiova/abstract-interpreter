c90ea3ad387144125f3a1e9f4320ba3690c562a6
eapiova <eapiova@gmail.com>
2022-10-31 10:09:20 +0100

    update

--- .Makefile.coq.d
+++ .Makefile.coq.d
@@ -4,6 +4,6 @@ library.vos library.vok library.required_vos: library.v
 abstract_interpreter.vo abstract_interpreter.glob abstract_interpreter.v.beautified abstract_interpreter.required_vo: abstract_interpreter.v library.vo
 abstract_interpreter.vio: abstract_interpreter.v library.vio
 abstract_interpreter.vos abstract_interpreter.vok abstract_interpreter.required_vos: abstract_interpreter.v library.vos
-extended_sign_examples.vo extended_sign_examples.glob extended_sign_examples.v.beautified extended_sign_examples.required_vo: extended_sign_examples.v abstract_interpreter.vo
-extended_sign_examples.vio: extended_sign_examples.v abstract_interpreter.vio
-extended_sign_examples.vos extended_sign_examples.vok extended_sign_examples.required_vos: extended_sign_examples.v abstract_interpreter.vos
+extended_sign_examples.vo extended_sign_examples.glob extended_sign_examples.v.beautified extended_sign_examples.required_vo: extended_sign_examples.v library.vo abstract_interpreter.vo
+extended_sign_examples.vio: extended_sign_examples.v library.vio abstract_interpreter.vio
+extended_sign_examples.vos extended_sign_examples.vok extended_sign_examples.required_vos: extended_sign_examples.v library.vos abstract_interpreter.vos
