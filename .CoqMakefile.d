79507e4a7c5d5d9ebc0061bf3789de058c7e7676
Riccardo Borsetto <eapiova@disroot.org>
2024-11-26 17:02:45 +0100

    update

--- .CoqMakefile.d
+++
@@ -1,4 +0,0 @@
-library.vo library.glob library.v.beautified library.required_vo: library.v 
-library.vos library.vok library.required_vos: library.v 
-abstract_interpreter.vo abstract_interpreter.glob abstract_interpreter.v.beautified abstract_interpreter.required_vo: abstract_interpreter.v library.vo
-abstract_interpreter.vos abstract_interpreter.vok abstract_interpreter.required_vos: abstract_interpreter.v library.vos
